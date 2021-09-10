73d4ec1c97b103605f199253c7874d07c229a8b1
pcygan44 <p.cygan44@gmail.com>
2021-09-10 13:54:39 +0100

    first commit

---
+++ src/pet_shop.py
@@ -0,0 +1,22 @@
+# WRITE YOUR FUNCTIONS HERE
+
+def get_pet_shop_name( shop):
+   
+    return shop["name"]
+
+
+def get_total_cash(shop):
+    
+    return  shop["admin"]["total_cash"]
+
+
+def add_or_remove_cash (shop, amount):
+    
+    shop["admin"]["total_cash"] += amount
+    
+
+
+
+
+
+    
